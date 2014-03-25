bce677132e7e2bb5e1241081e2b37778cbf740dc
Matt Chowning <matt.chowning@detroitlabs.com>
2014-03-24 22:15:36 -0400

    Minor update to tests to see about basic functionality.

--- SampleSpec.m
+++ SampleSpec.m
@@ -1,4 +1,5 @@
 #import "Kiwi.h"
+#import "CFRReviverWod.h"
 
 SPEC_BEGIN(MathSpec)
 
@@ -7,6 +8,14 @@ describe(@"Math", ^{
         NSUInteger a = 16;
         NSUInteger b = 26;
         [[theValue(a + b) should] equal:theValue(42)];
+    });
+});
+
+describe(@"test", ^{
+    CFRReviverWod *wod = [[CFRReviverWod alloc] init];
+    wod.title = @"Wod";
+    it(@"for wod title", ^{
+        [[wod.title should] containString:@"Wod"];
     });
 });
 
